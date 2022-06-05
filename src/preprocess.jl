f3c0e7688f089dee5e80c2e77f0a217e527f471b
rex <1073853456@qq.com>
2022-06-05 23:33:57 +0800

    data preprocess

---
+++ src/preprocess.jl
@@ -0,0 +1,9 @@
+# 数据预处理-Julia
+paths = Dict(
+    "ILLs_zh_en" => "../data/ILLs(zh-en).txt",
+    "train_data" => "../data/train_data.txt",
+    "triple_en" => "../data/triple_en.txt",
+    "triple_zh" => "../data/triple_zh.txt"
+)
+
+output_path = "../data/extract/"
