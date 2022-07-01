8d41647fa921cd48d774bdb0f68fda44dc6f6da1
rex <1073853456@qq.com>
2022-07-01 13:23:47 +0800

    更新路径推理的跨语言判断

--- src/deduction.jl
+++ src/deduction.jl
@@ -2,32 +2,25 @@
 using StringDistances
 using DataStructures
 
-"字典排序"
-function sort_count(list)
-    c_list = counter(list)
-    sort(Dict(c_list), by=i->c_list[i], rev=true)
-end
-# 索引工具
-take(ind::Int) = list->list[ind]
 
 """
     dist(target, word; char=true)
 
 计算 target 和 word 的距离:
- - 默认匹配字符距离，适用于问题匹配
- - 指定 `char=false` 将单词作为整体匹配，适用于实体匹配
+ - char 指定匹配方式，字符匹配适用于问题，单词匹配适用于实体
 """
 function dist(target, word; char=true)
-    # char 指定是否用字符匹配
     char || (target = split(target, '_'); word = split(word, '_'))
     all(∉(Set(target)), word) && return typemax(Int)
-    # 计算距离
     evaluate(Levenshtein(), target, word)
-    # evaluate(DamerauLevenshtein(), target, word)
 end
 
 
-"计算字串的 Levenshtein 距离"
+"""
+    nearby(words::AbstractVector, target; best=10, char)
+
+在 words 中匹配与 target 最近的前 best 个结果
+"""
 nearby(words::AbstractVector) = Base.Fix1(nearby, words)
 function nearby(words::AbstractVector, target; best=10, char=true)
     worddists = [dist(target, word; char=char) for word in words]
@@ -35,4 +28,61 @@ function nearby(words::AbstractVector, target; best=10, char=true)
     inds = sort(1:n, by= i->worddists[i])[1:min(best, n)]
     words[inds], worddists[inds]
 end
+findbest(words::AbstractVector, target; char=true) = argmin(i -> dist(target, i; char=char), words)
+
+
+"寻找子图，不存在的情况推理实体对齐"
+function find_vague_path(ner, rels)
+    length(rels) == 1 && throw("只匹配到一跳")
+    # 第一个三元组精确匹配
+    tri1 = filter(i->i[1]==ner && i[2]==rels[1], triples)
+    isempty(tri1) && throw("第一跳匹配错误")
+    tri1 = tri1[1]
+    
+    # 第二个三元组可能的头实体
+    possible = first.(filter(i->i[2] == rels[2], triples))
+    # 取最匹配的情况
+    sub = findbest(possible, tri1[3]; char=false)
+    tri2 = filter(i->i[1]==sub && i[2]==rels[2], triples)[1]
+    if length(rels) == 2
+        [tri1, tri2]
+    else
+        possible = first.(filter(i->i[2] == rels[3], triples))
+        sub = findbest(possible, tri2[3]; char=false)
+        tri3 = filter(i->i[1]==sub && i[2]==rels[3], triples)[1]
+        [tri1, tri2, tri3]
+    end
+end
+
+vague_submit(sol::AbstractString) = vague_submit(split(sol, '\t'))
+function vague_submit(sol::AbstractVector)
+    que, ner, rels = sol[1], sol[2], sol[3:end]
+    path = find_vague_path(ner, rels)
+    # println.(path) # 调试：打印路径
+    [submit_format(tris, sign) for (sign, tris) in valid_path(path)]
+end
+
+
+
+# 初始化查询字典
+train_rels = DefaultDict{String, Vector{Tuple}}(Vector{Tuple})
+for sols in unique(train_ques_sols)
+    que = first(sols)
+    rels = Tuple(@view(sols[3:end]))
+    rels ∉ train_rels[que] && push!(train_rels[que], rels)
+end
+"匹配最近 10 个问题"
+templates = unique(train_ques)
+top10 = nearby(templates)
+bestrel(que, ner) = train_rels[findbest(templates, que)][1]
+
+
+
+"字典排序"
+function sort_count(list)
+    c_list = counter(list)
+    sort(Dict(c_list), by=i->c_list[i], rev=true)
+end
+# 索引工具
+take(ind::Int) = list->list[ind]
 
--- src/submit.jl
+++ src/submit.jl
@@ -1,62 +1,12 @@
-# 路径搜索与数据提交格式
-
+# 路径搜索与标准格式生成
 using DataStructures
 
-# 初始化
-edges = DefaultDict{String, Vector{Tuple}}(Vector{Tuple})
-for (sub, rel, obj) in triples
-    push!(edges[sub], (rel, obj))
-end
-en_S, zh_S = Set(en_triples_lower), Set(zh_triples_lower)
-triple_lang(triple) = (triple ∈ en_S, triple ∈ zh_S)
+# 三元组还原
+en_standard = Dict((=>).(en_triples_lower, raw_en_triples))
+zh_standard = Dict((=>).(zh_triples_lower, raw_zh_triples))
 
-# 基本函数
-"问题-ner-若干关系 => 标准形式"
-function submit(sol)
-    # 提取信息
-    sol = split(sol, '\t')
-    que, ner, rels = sol[1], sol[2], sol[3:end]
-    # 获取问题解答
-    paths = filter(i->length(unique(i[1]))>1, find_paths(ner, rels))
-    if !isempty(paths)
-        sign, path = last(paths)
-    else
-        rels = find_rels(que)
-        sign, path = find_paths_vague(ner, rels)
-    end 
-    submit_format(path, sign)
-end
-
-"寻找子图，并返回相关的三元组"
-function find_paths(ner, rels)
-    paths = [[(ner, "", ner)]] # 初始只有一条路径
-    # 2 <= length(rels) <= 3 || return []
-    for rel in rels, _ in eachindex(paths)
-        path = popfirst!(paths) 
-        ner = path[end][end] # 取路径终点
-        haskey(edges, ner) || continue
-        for (newrel, obj) in edges[ner]
-            newrel == rel && push!(paths, vcat(path, (ner, newrel, obj)))
-        end
-    end
-    popfirst!.(paths)
-    vcat(standard_path.(paths)...)
-end
-
-"还原跨语言信息"
-function standard_path(path)
-    signs = [""]
-    for triple in path, _ in eachindex(signs)
-        s = popfirst!(signs)
-        triple ∈ zh_S && push!(signs, s * "z")
-        triple ∈ en_S && push!(signs, s * "e")
-    end
-    [[sign, [s == 'z' ? MT_zh_new2raw[t] : MT_en_new2raw[t] 
-            for (s,t) in zip(sign, path)]] for sign in signs]
-end
-
-"将三元组转为提交的标准形式"
-function standard_triple(triple, lang)
+"三元组 => 提交形式"
+function triple2link(triple, lang)
     sub, rel, obj = triple
     if lang == 'e'
         "<http://dbpedia.org/resource/$sub>#" * 
@@ -68,46 +18,52 @@ function standard_triple(triple, lang)
         "<http://zh.dbpedia.org/resource/$obj>"
     end
 end
-"将若干三元组转为提交形式"
-submit_format(triples, langs) = join([standard_triple(triple, lang)
+
+# 初始化边集
+edges = DefaultDict{String, Vector{Tuple}}(Vector{Tuple})
+for (sub, rel, obj) in triples
+    push!(edges[sub], (rel, obj))
+end
+
+"寻找从 ner 出发以 rels 为关系的子图（不做路径推理）"
+function find_paths(ner, rels)
+    paths = [[(ner, "", ner)]] # 初始路径
+    for rel in rels, _ in eachindex(paths)
+        path = popfirst!(paths) 
+        ner = path[end][end] # 取路径终点
+        for (newrel, obj) in edges[ner]
+            newrel == rel && push!(paths, vcat(path, (ner, newrel, obj)))
+        end
+    end
+    popfirst!.(paths) # 去除路径的初始节点（平凡）
+    paths
+end
+
+"翻译的三元组路径 => 原始三元组路径 + 三元组语言信息"
+function valid_path(path)
+    signs = [""]
+    # BFS 搜索可能的符号信息
+    for triple in path, _ in eachindex(signs)
+        s = popfirst!(signs) # 当前符号路径
+        haskey(zh_standard, triple) && push!(signs, s * "z")
+        haskey(en_standard, triple) && push!(signs, s * "e")
+    end
+    [[sign, [s == 'z' ? zh_standard[t] : en_standard[t] 
+            for (s,t) in zip(sign, path)]] for sign in signs if length(unique(sign)) > 1]
+end
+
+"三元组路径 => 提交形式"
+submit_format(triples, langs) = join([triple2link(triple, lang)
         for (triple, lang) in zip(triples, langs)], '#')
 
-
-### 以下为路径推理 ### 
-
-
-
-# 初始化查询字典
-train_rels = DefaultDict{String, Vector{Tuple}}(Vector{Tuple})
-for sols in unique(train_ques_sols)
-    que = first(sols)
-    rels = Tuple(@view(sols[3:end]))
-    rels ∉ train_rels[que] && push!(train_rels[que], rels)
-end
-"匹配最近 10 个问题"
-top10 = nearby(unique(train_ques))
-
-function find_rels(que)
-    ques, scores = top10(que)
-    rels = vcat([train_rels[que] for que in ques]...)
-    rel1 = first(sort_count(counter(first.(rels))))[1]
-    rel2 = first(sort_count(counter(take(2).(rels))))[1]
-    return [rel1, rel2]
-end
-
-
-"寻找子图，但推理对齐"
-function find_paths_vague(ner, rels)
-    tri1 = filter(i->i[1]==ner && i[2]==rels[1], triples)[1]
-    possible = filter(i->i[2] == rels[2], triples)
-    res, scores = nearby(first.(possible), tri1[3]; char=false)
-    tri2 = filter(i->i[1]==res[1] && i[2]==rels[2], triples)[1]
-    if length(rels) == 2
-        standard_path([tri1, tri2])[1]
-    else
-        possible = filter(i->i[2] == rels[3], triples)
-        res, scores = nearby(first.(possible), tri2[3]; char=false)
-        tri3 = filter(i->i[1]==res[1] && i[2]==rels[3], triples)[1]
-        standard_path([tri1, tri2, tri3])[1]
+"提取有效的解答"
+valid_submit(sol::AbstractString) = valid_submit(split(sol, '\t'))
+function valid_submit(sol::AbstractVector)
+    que, ner, rels = sol[1], sol[2], sol[3:end]
+    paths = find_paths(ner, rels)
+    valids = String[]
+    for path in paths, (sign, tris) in valid_path(path)
+        push!(valids, submit_format(tris, sign))
     end
+    valids
 end
