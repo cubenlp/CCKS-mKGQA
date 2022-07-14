87b3493cab378de421d37af70c8583e4d1a59494
rex <1073853456@qq.com>
2022-07-14 23:21:02 +0800

    更新路径推理

--- src/process/submitpath.jl
+++ src/process/submitpath.jl
@@ -69,24 +69,30 @@ precise_submit(ner, rels, edges, dict) = submit_format.(
 #     res
 # end
 
-"子图不存在时，推理对齐实体"
+"""
+    find_vague_paths(ner, rels, triples; best=2)
+
+推理对齐实体
+"""
 function find_vague_paths(ner, rels, triples; best = 2)
     # 跳数不够
     length(rels) == 1 && return [] # 只有一跳
     # 第一跳精确匹配
     tri1s = filter(i->i[1]==ner && i[2]==rels[1], triples)
-    # isempty(tri1s) && return [] # 找不到
-    isempty(tri1s) && (tri1s = filter(i->i[1]==ner, triples)) # 找不到时，更换关系
-    tri1 = tri1s[1]
+    # isempty(tri1s) && return [] # 找不到，返回无解
+    isempty(tri1s) && (tri1s = filter(i->i[1]==ner, triples)) # 找不到，更换关系
+    tri1 = tri1s[1] # 随机取一个
     # 第二跳模糊匹配
     possible = filter(i->i[2]==rels[2], triples)
-    tri2s = possible[nearby(first.(possible), tri1[3]; char=false, best=best,index=true)] # 字符匹配前 best 个
+    # 字符匹配最佳的 best 个
+    tri2s = possible[nearby(first.(possible), tri1[3]; char=false, best=best, index=true)]
     length(rels) == 2 && return [[tri1, tri] for tri in tri2s]
     # 第三跳模糊匹配
     possible = filter(i->i[2]==rels[3], triples)
     paths = []
     for tri2 in tri2s
-        for tri3 in possible[nearby(first.(possible), tri2[3],char=false, best=2,index=true)]
+        # 只考虑最近的两个
+        for tri3 in possible[nearby(first.(possible), tri2[3], char=false, best=2, index=true)]
             push!(paths, [tri1, tri2, tri3])
         end
     end
